2f899710e39443550d76c3963e4e9084e172d375
Andrii Kostetskyi <kostyandrew@gmail.com>
2017-11-29 20:51:11 +0200

    install commands

---
+++ install.sh
@@ -0,0 +1,20 @@
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install homebrew cask
+brew tap caskroom/cask
+
+#cask apps
+brew cask install google-chrome
+brew cask install firefox
+brew cask install telegram
+brew cask install phpstorm
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install slack
+brew cask install setapp
+brew cask install docker
+brew cask install grammarly
+brew cask install postman
+brew cask install skype
+brew cask install rescuetime
+brew cask install 1password
