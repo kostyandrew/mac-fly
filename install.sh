bc30d42b4a0de6aff0b5383dc11607dda00bf257
Andrii Kostetskyi <kostyandrew@gmail.com>
2019-08-22 15:20:17 +0300

    remove some programs

--- install.sh
+++ install.sh
@@ -4,17 +4,12 @@
 brew tap caskroom/cask
 
 #cask apps
-brew cask install google-chrome
 brew cask install firefox
 brew cask install telegram
 brew cask install phpstorm
 brew cask install visual-studio-code
 brew cask install iterm2
 brew cask install slack
-brew cask install setapp
 brew cask install docker
-brew cask install grammarly
 brew cask install postman
-brew cask install skype
-brew cask install rescuetime
 brew cask install 1password
