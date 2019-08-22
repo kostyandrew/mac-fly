b9ce3d1e6f3ffb115110d46783e164a9c96cebe2
Andrii Kostetskyi <kostyandrew@gmail.com>
2019-08-22 19:02:37 +0300

    Update mac-fly.sh

--- mac-fly.sh
+++ mac-fly.sh
@@ -1,4 +1,13 @@
+#projects
 [ -d ~/Projects ] && pj() { cd ~/Projects/$1}
 [ -d ~/Projects/My ] && pjm() { pj "My/$1"}
 [ -d ~/Projects/Work ] && pjw() { pj "Work/$1"}
 [ -d ~/Projects/Job ] && pjj() { pj "Job/$1"}
+[ -d ~/Projects/Forks ] && pjf() { pj "Forks/$1"}
+#git
+gut(){ echo "Oh My Gut\n" && git $@}
+пше(){ echo "Oh My Пше\n" && git $@}
+#docker-compose
+dc(){ docker-compose $@}
+dcu(){ docker-compose up -d $@}
+dcs(){ docker-compose stop $@}
